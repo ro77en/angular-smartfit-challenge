f24e8efeb4afeda09666447712c18b62a29cb9bf
ro77en <lucas.mdantas@icloud.com>
2024-06-17 10:38:03 -0300

    open units filter

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { GetUnitsService } from '../../services/get-units.service';
+import { Location } from '../../types/location.interface';
 
 @Component({
   selector: 'app-forms',
@@ -8,7 +9,8 @@ import { GetUnitsService } from '../../services/get-units.service';
   styleUrl: './forms.component.scss',
 })
 export class FormsComponent {
-  results = [];
+  results: Location[] = [];
+  filteredResults: Location[] = [];
   formGroup!: FormGroup;
 
   constructor(
@@ -17,15 +19,24 @@ export class FormsComponent {
   ) {}
 
   ngOnInit(): void {
-    this.unitService.getAllUnits().subscribe((data) => console.log(data));
     this.formGroup = this.formBuilder.group({
       hour: '',
-      showClosed: false,
+      showClosed: true,
+    });
+    this.unitService.getAllUnits().subscribe((data) => {
+      this.results = data.locations;
+      this.filteredResults = data.locations;
     });
   }
 
   onSubmit() {
-    console.log(this.formGroup.value);
+    if (!this.formGroup.value.showClosed) {
+      this.filteredResults = this.results.filter(
+        (location) => location.opened === true
+      );
+    } else {
+      this.filteredResults = this.results;
+    }
   }
 
   onClean() {
